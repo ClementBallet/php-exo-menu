1680f13c406fda1cb1d2829e13f6709f5b099c1f
Clément Ballet <clementballet@gmail.com>
2022-10-16 20:11:14 +0200

    Add files

---
+++ blog.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+
+<?php
+include 'template-parts/head.html';
+?>
+
+<body>
+<?php
+require 'template-parts/header.html';
+require 'template-parts/main.php';
+require 'template-parts/footer.html';
+?>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+
+<?php
+include 'template-parts/head.html';
+?>
+
+<body>
+<?php
+require 'template-parts/header.html';
+?>
+
+<h1>Contact</h1>
+
+<?php
+require 'template-parts/footer.html';
+?>
+</body>
+
+</html>
